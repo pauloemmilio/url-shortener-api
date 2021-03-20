92d3fed46f6edf6961c5e4c41a67c76830243615
Paulo Emílio <paulo.emilio@dcx.ufpb.br>
2021-03-20 16:24:11 -0300

    Initial configuration

---
+++ src/test/kotlin/com/paulo/urlshortner/UrlShortnerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.paulo.urlshortner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
